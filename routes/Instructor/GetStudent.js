01ecd79e9415e0c7c15025bbf091379c96c5be35
mohammed21kamal <112828059+mohammed21kamal@users.noreply.github.com>
2023-04-26 08:30:43 +0200

    Add files via upload

---
+++ routes/Instructor/GetStudent.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const conn = require("../../db/dbConnection");
+const util = require("util");
+
+router.get("/:id", async (req, res) => {
+  try {
+    const query = util.promisify(conn.query).bind(conn);
+    let search = "";
+
+    const instructor = await query(
+      "SELECT * FROM course_instructors WHERE instructorId = ?",
+      [req.params.id]
+    );
+
+    if (!instructor[0]) {
+      return res.status(404).json({ msg: "instructor not found!" });
+    }
+
+    if (req.query.search) {
+      search = ` AND (course_instructors.courseId LIKE ?)`;
+    }
+
+    const getStudent = await query(
+        `SELECT users.name, course.name AS course_name, enrollment.grade , course.code
+        FROM enrollment JOIN course ON course.id = enrollment.courseId 
+        JOIN users ON users.id = enrollment.studentId
+        JOIN course_instructors ON course_instructors.courseId = enrollment.courseId
+        WHERE course_instructors.instructorId = ?
+        ${search}`,
+        [req.params.id]
+      );
+      
+    res.status(200).json({ getStudent });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
